0b1898bb3c078328064b8543ab7b478dd237eed5
Prashun Javeri <origamyllc@gmail.com>
2016-09-24 16:52:35 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 const redis = require("./redisEngine.js");
 const memory = require("./memoryEngine.js");
-module.exports = memory;
-if ( process.env.CACHE_TYPE &&  process.env.CACHE_TYPE === "redis" ){
-    module.exports = redis;
-}
+module.exports.memory = memory;
+module.exports.redis = redis;
 
 
 
